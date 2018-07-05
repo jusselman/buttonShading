c7eb040940f4612ab5297b0e52abcd5e64308beb
jusselman <usselmanjosh@gmail.com>
2018-07-04 20:50:42 -0700

    shading buttons

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var conversion = function converter(f) {
+  return ( 5/9 ) * (f - 32);
+}
+
+var userInput = document.getElementById('userInput').innerHTML;
+
+function converter() {
+document.getElementById('converter').innerHTML = ;
+}
